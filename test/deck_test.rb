9f2d4e7811b35980e60f1693b780f7107ec499b4
James Belta <james.belta@gmail.com>
2020-05-06 16:01:17 -0600

    Build ranking of card test

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -14,4 +14,25 @@ class DeckTest < Minitest::Test
     assert_instance_of Deck, deck
   end
 
+  def test_it_has_attributes
+    card1  = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    assert_equal [card1, card2, card3], deck.cards
+  end
+
+  def test_ranking_of_card
+    card1  = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    assert_equal 12, deck.rank_of_card_at(0)
+    assert_equal 14, deck.rank_of_card_at(2)
+
+  end
 end
