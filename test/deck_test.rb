2d8a3c5a83f2112eb24e386bbca6bb68e39db55a
James Belta <james.belta@gmail.com>
2020-05-06 16:54:49 -0600

    Build add_card test

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -67,16 +67,17 @@ class DeckTest < Minitest::Test
     assert_equal 50.0, deck.percent_high_ranking
   end
 
-#  def test_add_card
-#    card1  = Card.new(:diamond, 'Queen', 12)
-#    card2 = Card.new(:spade, '3', 3)
-#    card3 = Card.new(:heart, 'Ace', 14)
-#    card4 = Card.new(:club, '5', 5)
-#    cards = [card1, card2, card3]
-#    deck = Deck.new(cards)
-#
-#    deck.remove_card
-#    assert_equal [card2,card3], deck.cards
-#  end
+  def test_add_card
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    card4 = Card.new(:club, '5', 5)
+    cards = [card2, card3]
+    deck = Deck.new(cards)
+    deck.add_card(card4)
+
+    assert_equal [card2, card3, card4], deck.cards
+
+
+  end
 
 end
