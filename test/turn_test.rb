a8208c47c7c84b0c67fa4eee44ba6511342a29c4
James Belta <james.belta@gmail.com>
2020-05-07 14:51:07 -0600

    test_type and type method

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -25,4 +25,14 @@ class TurnTest < Minitest::Test
   def test_it_exist
     assert_instance_of Turn, @turn
   end
+
+  def test_it_has_attributes
+    assert_equal @player1, @turn.player1
+    assert_equal @player2, @turn.player2
+    assert_equal [], @turn.spoils_of_war
+  end
+
+  def test_type
+    assert_equal :basic, @turn.type
+  end
 end
